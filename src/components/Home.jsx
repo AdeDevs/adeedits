8cf0e5a0bc073cb9f9cbd8a8d1976f339fa59e57
AdeDevs <adeyemiakinyemi01@gmail.com>
2025-11-07 14:50:48 +0100

    update gallery and home styles; replace images with webp format and add background colors

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,16 @@
 import { NavLink } from "react-router-dom";
+import blurry from "../assets/esther.webp";
+import winterBg from "../assets/mayo.webp";
+import potrait from "../assets/smug.webp";
+import road from "../assets/road.jpg";
+import bridge from "../assets/bridge.webp";
+import mosque from "../assets/mosque.webp";
+import morning from "../assets/morning.webp";
+import pose from "../assets/pose.webp";
+import ayo from "../assets/ayo.webp";
+import face from "../assets/face.webp";
+import close from "../assets/close-up.webp";
+import gaff from "../assets/gaff.webp";
 
 export default function HomePage() {
     return (
@@ -27,13 +39,19 @@ export default function HomePage() {
                                 <p>
                                 Portraits can turn ordinary spaces into quiet stages. This series focuses on presence — the texture, the expression, the moment. Three frames, simple but intentional. As my work grows, the story will too. For now, it just needs to feel true.
                                 </p>
-                                <p><NavLink to="/">view full gallery</NavLink></p>
-                                <div className="winter-blurry"></div>
+                                <p><NavLink to="/gallery">view full gallery</NavLink></p>
+                                <div className="winter-blurry">
+                                    <img src={blurry} alt="model" />
+                                </div>
                             </div>
                         </div>
-                        <div className="winter-bg"></div>
+                        <div className="winter-bg">
+                            <img src={winterBg} alt="model" />
+                        </div>
                     </div>
-                    <div className="winter-potrait"></div>
+                    <div className="winter-potrait">
+                        <img src={potrait} alt="model" />
+                    </div>
                 </section>
                 <div className="fall-collection">
                     <div className="item item-1">
@@ -42,14 +60,24 @@ export default function HomePage() {
                             <p>
                             Portraits are little worlds of their own — simple expressions that suddenly feel deeper when you’re close enough. I try not to force anything; I just follow the moment and let it speak. This set brings together five close-up shots of three models, each one holding a quiet piece of who they are.
                             </p>
-                            <p><a href="#">View full gallery</a></p>
+                            <p><NavLink href="/gallery">View full gallery</NavLink></p>
                         </div>
                     </div>
-                    <div className="item item-2"></div>
-                    <div className="item item-3"></div>
-                    <div className="item item-4"></div>
-                    <div className="item item-5"></div>
-                    <div className="item item-6"></div>
+                    <div className="item item-2">
+                        <img src={pose} alt="model" />
+                    </div>
+                    <div className="item item-3">
+                        <img src={ayo} alt="model" />
+                    </div>
+                    <div className="item item-4">
+                        <img src={face} alt="model" />
+                    </div>
+                    <div className="item item-5">
+                        <img src={close} alt="model" />
+                    </div>
+                    <div className="item item-6">
+                        <img src={gaff} alt="model" />
+                    </div>
                 </div>
                 <div className="still-collection">
                     <div className="still still-1">
@@ -58,13 +86,21 @@ export default function HomePage() {
                             <p>
                                 Photos are metaphors, making the abstract concrete and mystifying the mundane. Be clear, be confident, and don’t overthink it. The beauty of your story is that it’s going to continue to evolve and your site can evolve with it. Your goal should be to make it feel right for right now. Later will take care of itself.
                             </p>
-                            <p><a href="#">View full gallery</a></p>
+                            <p><NavLink href="/gallery">View full gallery</NavLink></p>
                         </div>
                     </div>
-                    <div className="still still-2"></div>
-                    <div className="still still-3"></div>
-                    <div className="still still-4"></div>
-                    <div className="still still-5"></div>
+                    <div className="still still-2">
+                        <img src={road} alt="model" />
+                    </div>
+                    <div className="still still-3">
+                        <img src={bridge} alt="model" />
+                    </div>
+                    <div className="still still-4">
+                        <img src={mosque} alt="model" />
+                    </div>
+                    <div className="still still-5">
+                        <img src={morning} alt="model" />
+                    </div>
                 </div>
             </main>
         </div>
